64d3293f6d3b2e9029c69fac36a1af40de78b009
ergiom <ergiom1@gmail.com>
2021-09-08 22:36:33 +0200

    added user prompt for size

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-function createDivs() {
-    let body = document.querySelector('body');
-    for (let i = 0; i < 16 * 16; i++) {
+function createDivs(size) {
+    let main_grid = document.querySelector('#main-grid-div');
+    main_grid.style['grid-template-columns'] = `repeat(${size}, 1fr)`
+    for (let i = 0; i < size * size; i++) {
         let div = document.createElement('div');
         div.classList.add('grid-div');
-        body.appendChild(div);
+        main_grid.appendChild(div);
     }
 }
 
@@ -13,8 +14,21 @@ function changeColor(event) {
     div.style.backgroundColor = 'black';
 }
 
+function sizeInput() {
+    while (true) {
+        let size = Number(prompt('specify size:'));
+        if (size > 0 && size < 100) return size;
+    }
+}
+
 function initialize() {
-    createDivs();
+    let restart = document.querySelector('#reset-button');
+    restart.addEventListener('click', initialize);
+    let size = sizeInput();
+
+    let grid_div = document.querySelector('#main-grid-div');
+    grid_div.innerHTML = '';
+    createDivs(size);
     let divs = Array.from(document.querySelectorAll('.grid-div'));
     console.log(divs);
     divs.forEach((div) =>
